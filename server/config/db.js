d9ab8177daabd569ba550f41b38a9558716b27e7
Shahaf Shaked <shahaf@Shahafs-MacBook-Pro.local>
2016-05-06 07:49:58 +0300

    change db ip

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connect() {
-  mongoose.connect('mongodb://192.168.99.100:27017/Contacts');
+  mongoose.connect('mongodb://localhost:27017/Contacts');
 }
 
 module.exports = connect;
